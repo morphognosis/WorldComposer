edceddc9a940a694e181b0c605d85ca9f0636804
portegys <portegys@gmail.com>
2022-05-27 19:45:36 -0500

    Baseline performance

--- work/tabulate_no_train.sh
+++ work/tabulate_no_train.sh
@@ -4,15 +4,14 @@ echo basePathLength,numModularPaths = $prefix
 for mod in insert substitute delete all
 do
 echo modification = $mod
-for file in $(ls world_composer_*nn*_test_results.csv | grep -v no_train)
+for file in $(ls world_composer_no_train_*nn*_test_results.csv)
 do
 f=$(basename $file .csv)
 f=${f/_test_results/}
 f=$(echo $f | cut -c16-)
 echo -n $f ": "
-nn128=$(grep ^${prefix},128,${mod} $file | cut -d"," -f10 | awk '{sum += $0; count += 1} END {print sum/count}')
 nn256=$(grep ^${prefix},256,${mod} $file | cut -d"," -f10 | awk '{sum += $0; count += 1} END {print sum/count}')
-python -c "print('128 neurons:', ${nn128}, '256 neurons:', ${nn256}, 'delta: ', ${nn128} - ${nn256})"
+python -c "print(${nn256})"
 done
 done
 done
@@ -22,15 +21,14 @@ echo basePathLength,numModularPaths = $prefix
 for mod in insert substitute delete all
 do
 echo modification = $mod
-for file in $(ls world_composer_tcn_test_results.csv)
+for file in $(ls world_composer_no_train_tcn_test_results.csv)
 do
 f=$(basename $file .csv)
 f=${f/_test_results/}
 f=$(echo $f | cut -c16-)
 echo -n $f ": "
 k2=$(grep ^${prefix},2,${mod} $file | cut -d"," -f10 | awk '{sum += $0; count += 1} END {print sum/count}')
-k4=$(grep ^${prefix},4,${mod} $file | cut -d"," -f10 | awk '{sum += $0; count += 1} END {print sum/count}')
-python -c "print('kernel 2:', ${k2}, 'kernel 4:', ${k4}, 'delta: ', ${k2} - ${k4})"
+python -c "print(${k2})"
 done
 done
 done
